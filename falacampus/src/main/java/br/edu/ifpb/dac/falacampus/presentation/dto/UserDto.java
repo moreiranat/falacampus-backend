844013056f2b676245e0d74d238e74f2c506627f
moreiranat <natalylucena.pb@gmail.com>
2022-04-14 19:36:44 -0300

    Atualização UserDto

--- falacampus/src/main/java/br/edu/ifpb/dac/falacampus/presentation/dto/UserDto.java
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/presentation/dto/UserDto.java
@@ -1,6 +1,72 @@
 package br.edu.ifpb.dac.falacampus.presentation.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.edu.ifpb.dac.falacampus.model.entity.User;
+
 public class UserDto {
 	
+	private Long id;
+	private String name;
+	private String email;
+	private Long registration;
+	private String departamentName;
 	
+	public UserDto() {
+		
+	}
+	
+	public UserDto(User user) {
+		this.id = user.getId();
+		this.name = user.getName();
+		this.email = user.getEmail();
+		this.registration = user.getRegistration();
+		this.departamentName = user.getDepartament().getName();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getRegistration() {
+		return registration;
+	}
+
+	public void setRegistration(Long registration) {
+		this.registration = registration;
+	}
+
+	public String getDepartamentName() {
+		return departamentName;
+	}
+
+	public void setDepartamentName(String departamentName) {
+		this.departamentName = departamentName;
+	}
+
+	public static List<UserDto> converter(List<User> users) {
+		return users.stream().map(UserDto::new).collect(Collectors.toList());
+	}
+
 }
