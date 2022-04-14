6623d162f7477adc2a78ce2715f56d2096113f0e
moreiranat <natalylucena.pb@gmail.com>
2022-04-14 00:47:01 -0300

    Initial commit

---
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/FalaCampusApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.falacampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaCampusApplication.class, args);
+	}
+
+}
