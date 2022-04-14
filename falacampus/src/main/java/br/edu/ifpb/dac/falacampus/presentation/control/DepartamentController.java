e3a540f50e0f13d1eb1a84541e5f45e9ffab6e4a
moreiranat <natalylucena.pb@gmail.com>
2022-04-14 11:29:33 -0300

    Pacotes presentation.dto e presentation.control

---
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/presentation/control/DepartamentController.java
@@ -0,0 +1,10 @@
+package br.edu.ifpb.dac.falacampus.presentation.control;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/departament")
+public class DepartamentController {
+
+}
