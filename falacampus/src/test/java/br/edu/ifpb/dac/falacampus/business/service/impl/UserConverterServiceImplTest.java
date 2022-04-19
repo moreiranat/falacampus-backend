3d97f6cd447ec11e8d4f7fa47e51cafd601ef34b
Patricia <patricia.pereira@academico.ifpb.edu.br>
2022-04-19 12:19:31 -0300

    Testes

---
+++ falacampus/src/test/java/br/edu/ifpb/dac/falacampus/business/service/impl/UserConverterServiceImplTest.java
@@ -0,0 +1,44 @@
+package br.edu.ifpb.dac.falacampus.business.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.edu.ifpb.dac.falacampus.business.service.DepartamentConverterService;
+import br.edu.ifpb.dac.falacampus.business.service.DepartamentService;
+import br.edu.ifpb.dac.falacampus.model.entity.Departament;
+import br.edu.ifpb.dac.falacampus.presentation.control.DepartamentController;
+import br.edu.ifpb.dac.falacampus.presentation.dto.DepartamentDto;
+
+class UserConverterServiceImplTest {
+
+	/*
+	 * DepartamentConverterService departamentConvertServiceImpl; DepartamentService
+	 * departamentService; DepartamentDto dto; DepartamentController
+	 * departamentController; Departament departament ;
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		/*
+		 * departamentController = new DepartamentController();
+		 * departamentConvertServiceImpl = new DepartamentConverterServiceImpl();
+		 * departamentService = new DepartamentService(); dto = new DepartamentDto();
+		 * departament = new Departament();
+		 */
+
+	}
+
+	@Test
+	void test() {
+	/*	 dto.setId( 1L);
+		 dto.setName("Biblioteca");
+		assertEquals(dto, departamentController.save(dto));  
+		
+		
+		//assertEquals(4, departamentController.save(dto), "The optional failure message is now the last parameter");
+		//assertTrue('a' < 'b', () -> "Assertion messages can be lazily evaluated -- "
+				//+ "to avoid constructing complex messages unnecessarily.");
+	}*/
+	}
+}
---
+++ falacampus/src/test/java/br/edu/ifpb/dac/falacampus/presentation/control/UserControllerTest.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.dac.falacampus.presentation.control;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
