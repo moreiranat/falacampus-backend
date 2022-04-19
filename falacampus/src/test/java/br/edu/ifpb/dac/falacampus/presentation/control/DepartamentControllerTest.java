addf6a792fe49619b43c0a744eb3ced3b515e287
rosenatobarreto <rosenatoblima@gmail.com>
2022-04-19 10:54:36 -0300

    Inciando testes

---
+++ falacampus/src/test/java/br/edu/ifpb/dac/falacampus/presentation/control/DepartamentControllerTest.java
@@ -0,0 +1,53 @@
+package br.edu.ifpb.dac.falacampus.presentation.control;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.edu.ifpb.dac.falacampus.business.service.DepartamentConverterService;
+import br.edu.ifpb.dac.falacampus.business.service.DepartamentService;
+import br.edu.ifpb.dac.falacampus.business.service.impl.DepartamentConverterServiceImpl;
+import br.edu.ifpb.dac.falacampus.model.entity.Departament;
+import br.edu.ifpb.dac.falacampus.presentation.dto.DepartamentDto;
+
+class DepartamentControllerTest {
+
+	DepartamentConverterService departamentConvertServiceImpl = new DepartamentConverterServiceImpl();
+	
+	DepartamentService departamentService = new DepartamentService();
+	
+	DepartamentDto dto; 
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void saveDepartamentTest() {
+		
+		dto = new DepartamentDto();
+		dto.setId(1L);
+		dto.setName("Maria");
+		
+	    assertEquals(dto, dto);
+		
+	}
+
+}
