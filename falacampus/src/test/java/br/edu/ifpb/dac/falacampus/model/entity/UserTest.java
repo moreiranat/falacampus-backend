e041da1d4998730b1ad12b8518ab754ad44bb463
Patricia <patricia.pereira@academico.ifpb.edu.br>
2022-04-20 19:43:47 -0300

    Tesetes no Departament e User

---
+++ falacampus/src/test/java/br/edu/ifpb/dac/falacampus/model/entity/UserTest.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.dac.falacampus.model.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
