3832f6dd3fb922979607b15bcbefdd29ef1a079e
Rosenato Barreto de Lima <201815020003@ad.ifpb.edu.br>
2022-04-20 18:55:10 -0300

    Atualização testes

--- falacampus/src/test/java/br/edu/ifpb/dac/falacampus/business/service/impl/UserConverterServiceImplTest.java
+++ falacampus/src/test/java/br/edu/ifpb/dac/falacampus/business/service/impl/UserConverterServiceImplTest.java
@@ -16,7 +16,7 @@ import br.edu.ifpb.dac.falacampus.presentation.dto.UserDto;
 
 class UserConverterServiceImplTest {
 
-	  
+	private static UserConverterService userConverterServiceImpl;  
 	private static UserConverterService userConverterService;
     private static UserDto dto;
     private static User user;
@@ -24,26 +24,28 @@ class UserConverterServiceImplTest {
 
     @BeforeAll
     static void setUpBeforeClass() throws Exception {
+    	
+    	userConverterServiceImpl = new UserConverterServiceImpl();
+    	user = new User();
+    	dto = new UserDto();
         
         dep = new Departament(1L, "Biblioteca", null);
         userConverterService = new UserConverterServiceImpl();
-        user = new User();
         
         user.setId(1L);
         user.setName("Maria");
         user.setEmail("maria@email.com");
         user.setRegistration(1234L);
         user.setRole(Role.STUDENT);
-        user.setPassword(null);
+        user.setPassword("1234");
         user.setDepartament(dep);
         
-        dto = new UserDto();
         dto.setId(1L);
         dto.setName("Maria");
         dto.setEmail("maria@email.com");
         dto.setRegistration(1234L);
         dto.setRole(Role.STUDENT);
-        dto.setPassword("123456");
+        dto.setPassword("1234");
         dto.setDepartamentId(dep.getId().longValue());
     }
 
@@ -59,32 +61,48 @@ class UserConverterServiceImplTest {
     @AfterEach
     void tearDown() throws Exception {
     }
+    
+    @Test
+    void userToDTOTest() {    
+    
+		assertEquals(UserDto.class, userConverterServiceImpl.userToDTO(user).getClass());
+    
+    }
+    
+    @Test
+    void dtoToUserTest() {    
+    
+        assertEquals(user.getClass(), userConverterServiceImpl.dtoToUser(dto).getClass());
+    
+    }
 
     @Test
-    public void userToDto() {
-        UserDto dtoConverted = userConverterService.userToDTO(user);
-        assertAll("Testing user to dto",
-            () -> assertEquals(dtoConverted.getId(), user.getId()),
-            () -> assertEquals(dtoConverted.getName(), user.getName()),
-            () -> assertEquals(dtoConverted.getEmail(), user.getEmail()),
-            () -> assertEquals(dtoConverted.getRegistration(), user.getRegistration()),
-            () -> assertEquals(dtoConverted.getRole(), user.getRole()),
-            () -> assertEquals(dtoConverted.getPassword(), user.getPassword()),
-            () -> assertEquals(dtoConverted.getDepartamentId().longValue(), user.getDepartament())
+    public void userToDtoItens() {
+        UserDto userDto = userConverterService.userToDTO(user);
+        assertAll("userToDto",
+            () -> assertEquals(userDto.getId(), user.getId()),
+            () -> assertEquals(userDto.getName(), user.getName()),
+            () -> assertEquals(userDto.getEmail(), user.getEmail()),
+            () -> assertEquals(userDto.getRegistration(), user.getRegistration()),
+            () -> assertEquals(userDto.getRole(), user.getRole()),
+            () -> assertEquals(userDto.getPassword(), user.getPassword()),
+            () -> assertEquals(userDto.getDepartamentId().longValue(), user.getDepartament())
         );
     }
     
     @Test
-    public void dtoToUser() {
-        User userConverted = userConverterService.dtoToUser(dto);
-        assertAll("Testing dto to user",
-                () -> assertEquals(userConverted.getId(), dto.getId()),
-                () -> assertEquals(userConverted.getName(), dto.getName()),
-                () -> assertEquals(userConverted.getEmail(), dto.getEmail()),
-                () -> assertEquals(userConverted.getRegistration(), dto.getRegistration()),
-                () -> assertEquals(userConverted.getRole(), dto.getRole()),
-                () -> assertEquals(userConverted.getPassword(), dto.getPassword()),
-                () -> assertEquals(userConverted.getDepartament(), dto.getDepartamentId())
+    public void dtoToUserItens() {
+        User user = userConverterService.dtoToUser(dto);
+        assertAll("dtoToUser",
+                () -> assertEquals(user.getId(), dto.getId()),
+                () -> assertEquals(user.getName(), dto.getName()),
+                () -> assertEquals(user.getEmail(), dto.getEmail()),
+                () -> assertEquals(user.getRegistration(), dto.getRegistration()),
+                () -> assertEquals(user.getRole(), dto.getRole()),
+                () -> assertEquals(user.getPassword(), dto.getPassword()),
+                () -> assertEquals(user.getDepartament(), dto.getDepartamentId())
         );
     }
+    
+
 }
