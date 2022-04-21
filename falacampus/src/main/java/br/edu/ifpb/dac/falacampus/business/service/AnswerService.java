7252674c65b76e325cf764278c872159e7c77612
Patricia <patricia.pereira@academico.ifpb.edu.br>
2022-04-21 16:11:46 -0300

    ...

--- falacampus/src/main/java/br/edu/ifpb/dac/falacampus/business/service/AnswerService.java
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/business/service/AnswerService.java
@@ -1,8 +1,11 @@
 package br.edu.ifpb.dac.falacampus.business.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.edu.ifpb.dac.falacampus.model.entity.Comment;
 import br.edu.ifpb.dac.falacampus.model.repository.AnswerRepository;
 
 @Service
@@ -10,5 +13,39 @@ public class AnswerService {
 	
 	@Autowired
 	private AnswerRepository answerRepository;
+	
+	public Comment save(Comment comment) {
+		return null;
+	}
+	
+	public void deleteById(Long id) {
+		
+	
+	}
+
+	public Comment update(Comment comment) {
+		return null;
+	}
+	
+	public Comment update(Long id) {
+				
+		return null;
+	}
+	
+	public Comment findById(Long id) {
+		return null;
+		
+	}
+
+	public Iterable<Comment> findAll() {
+		return null;
+	}
+	
+	public List<Comment> find(Comment filter) {
+		
+		return null;
+	}
+
+
 
 }
