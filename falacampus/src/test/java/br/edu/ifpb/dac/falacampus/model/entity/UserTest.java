5f2ec609de505f37234be17557890e9ec0c5cb34
moreiranat <natalylucena.pb@gmail.com>
2022-05-04 16:20:01 -0300

    Testes nos Services

--- falacampus/src/test/java/br/edu/ifpb/dac/falacampus/model/entity/UserTest.java
+++ falacampus/src/test/java/br/edu/ifpb/dac/falacampus/model/entity/UserTest.java
@@ -1,25 +1,35 @@
 package br.edu.ifpb.dac.falacampus.model.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Disabled;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullSource;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import br.edu.ifpb.dac.falacampus.business.service.UserService;
+import br.edu.ifpb.dac.falacampus.model.repository.UserRepository;
+
+@RunWith(MockitoJUnitRunner.class)
 class UserTest {
-	/*
-	 * @Test void test() { l("Not yet implemented"); }
-	 */
-	
 	
 	
 	@Test
 	@DisplayName("╯°□°）╯")
 	void testNameCharacters() {
 	}
-	
+
 	@Test
 	@DisplayName("😱")
 	void testNameContainingEmoji() {
