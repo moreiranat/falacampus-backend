b0a7f8ed2233d787785dc27fb7d695fa85b8c06d
Nataly Lucena <natalylucena.pb@gmail.com>
2022-05-26 10:56:08 -0300

    Update class Application to make requests using axios

--- falacampus/src/main/java/br/edu/ifpb/dac/falacampus/FalaCampusApplication.java
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/FalaCampusApplication.java
@@ -2,11 +2,21 @@ package br.edu.ifpb.dac.falacampus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FalaCampusApplication {
+@EnableWebMvc
+public class FalaCampusApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(FalaCampusApplication.class, args);
 	}
-
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+			.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
+	}
 }
