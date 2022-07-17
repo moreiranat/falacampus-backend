3ff7b53e7f4d915e8b3e64021f2756866481b246
santospatricia11 <patricia.pereira@academico.ifpb.edu.br>
2022-07-17 02:01:37 -0300

    Últimas Alterações

--- falacampus/src/main/java/br/edu/ifpb/dac/falacampus/business/service/AuthenticationService.java
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/business/service/AuthenticationService.java
@@ -3,7 +3,7 @@ package br.edu.ifpb.dac.falacampus.business.service;
 import br.edu.ifpb.dac.falacampus.model.entity.SystemUser;
 
 public interface AuthenticationService {
-	
+	//teste
 	public String loginLocal (String email,String password);
 	public SystemUser getLoggedUser();
 
