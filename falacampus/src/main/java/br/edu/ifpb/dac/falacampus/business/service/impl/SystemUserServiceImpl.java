e286b90e73ecb3f66f2786d25a13bb5145b141ad
santospatricia11 <patricia.pereira@academico.ifpb.edu.br>
2022-07-18 13:48:43 -0300

    Implemetação de métedos no SystemUserServiceImpl

--- falacampus/src/main/java/br/edu/ifpb/dac/falacampus/business/service/impl/SystemUserServiceImpl.java
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/business/service/impl/SystemUserServiceImpl.java
@@ -3,7 +3,10 @@ package br.edu.ifpb.dac.falacampus.business.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.criterion.Example;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -58,39 +61,56 @@ public class SystemUserServiceImpl implements SystemUserService {
 	}
 
 	@Override
-	public SystemUser delete(SystemUser systemUser) {
-		// TODO Auto-generated method stub
-		return null;
+	public SystemUser delete(Long id) {
+		if(id ==null) {
+			throw new IllegalStateException("Id connot be null");
+		}
+		SystemUser systemUser = systemUserRepository.findById(id).get();
+		if(id == null) {
+			throw  new IllegalStateException(String.format("Coult not find a entity", id)); 
+		}
+		systemUserRepository.delete(systemUser);
+		return systemUser;
 	}
 
 	@Override
 	public SystemUser findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		if(id==null){
+			throw new IllegalStateException("Id count be null");
+		}
+		return findById(id);
 	}
 
 	@Override
 	public SystemUser findByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		if(email==null){
+			throw new IllegalStateException("Email count be null");
+		}
+		return findByEmail(email);
 	}
 
 	@Override
 	public SystemUser findByUserName(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		if(username==null){
+			throw new IllegalStateException("UserName count be null");
+		}
+		return findByUserName(username);
 	}
 
 	@Override
 	public Iterable<SystemUser> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return systemUserRepository.findAll();
 	}
 
 	@Override
 	public List<SystemUser> find(SystemUser filter) {
-		// TODO Auto-generated method stub
-		return null;
+		Example example = Example.of(filter,
+				ExampleMatcher
+				.matching()
+				.withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
+		
+		return systemUserRepository.findAll();
 	}
 	
 
