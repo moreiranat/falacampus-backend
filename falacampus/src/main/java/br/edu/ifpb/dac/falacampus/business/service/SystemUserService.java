0ec01a9fbcfa4a192fd1054c3433783a3272d619
Nataly Lucena <natalylucena.pb@gmail.com>
2022-07-17 23:42:03 -0300

    Implementação do System User Service

---
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/business/service/SystemUserService.java
@@ -0,0 +1,66 @@
+package br.edu.ifpb.dac.falacampus.business.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+
+import br.edu.ifpb.dac.falacampus.model.entity.SystemUser;
+import br.edu.ifpb.dac.falacampus.model.repository.SystemUserRepository;
+
+public class SystemUserService {
+	
+	@Autowired
+	private SystemUserRepository systemUserRepository;
+	
+	public SystemUser save(SystemUser systemUser) {
+		return systemUserRepository.save(systemUser);
+	}
+
+	public void deleteById(Long id) {
+
+		SystemUser systemUser = findById(id);
+
+		if(systemUser == null) {
+			throw new IllegalStateException(String.format("Could not find a entity with id=%1", id));
+		}
+
+		systemUserRepository.deleteById(id);
+	}
+
+	public SystemUser update(SystemUser systemUser) {
+		return systemUserRepository.save(systemUser);
+	}
+
+	public SystemUser update(Long id) {
+		SystemUser systemUserSave = systemUserRepository.getById(id);
+		if(id == null) {
+			throw new IllegalStateException("Id cannot be null");
+		}		
+		return systemUserRepository.save(systemUserSave);
+	}
+
+	public SystemUser findById(Long id) {
+
+		if(id == null) {
+			throw new IllegalStateException("Id cannot be null");
+		}
+		return systemUserRepository.findById(id).get();
+	}
+
+	public List<SystemUser> findAll() {
+		return systemUserRepository.findAll();
+	}
+
+	public List<SystemUser> find(SystemUser filter) {
+		Example example = Example.of(filter, ExampleMatcher.matching()
+				.withIgnoreCase()
+				.withStringMatcher(StringMatcher.CONTAINING));
+
+		return systemUserRepository.findAll(example); 
+
+	}
+
+}
