eba4965040df5e2b067bcff87ed7d79ef0940f4a
Nataly Lucena <natalylucena.pb@gmail.com>
2022-07-26 23:41:14 -0300

    Atualização do Fala Campus Application

--- falacampus/src/main/java/br/edu/ifpb/dac/falacampus/FalaCampusApplication.java
+++ falacampus/src/main/java/br/edu/ifpb/dac/falacampus/FalaCampusApplication.java
@@ -1,14 +1,20 @@
 package br.edu.ifpb.dac.falacampus;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import br.edu.ifpb.dac.falacampus.business.service.SystemRoleService;
+
 @SpringBootApplication
 @EnableWebMvc
-public class FalaCampusApplication implements WebMvcConfigurer {
+public class FalaCampusApplication implements WebMvcConfigurer, CommandLineRunner {
+	
+	private SystemRoleService systemRoleService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FalaCampusApplication.class, args);
 	}
@@ -18,5 +24,11 @@ public class FalaCampusApplication implements WebMvcConfigurer {
 		registry
 			.addMapping("/**")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		systemRoleService.createDefaultValues();
+		
 	}
 }
